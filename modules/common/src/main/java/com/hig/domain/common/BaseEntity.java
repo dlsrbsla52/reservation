97fded3a272350e162cbf552d5d092d433848800
dlsrbsla52 <32616025+dlsrbsla52@users.noreply.github.com>
2026-03-10 18:33:32 +0900

    리팩토링: BaseEntity의 UUID 생성기 스타일 업데이트
    - ID 일관성을 위해 `@UuidGenerator`가 `UuidGenerator.Style.TIME`을 사용하도록 변경했습니다.
    - 이를 통해 DB의 채번이 아닌 어플리케이션단의 PK 채번으로 부하를 낮추었습니다.
    - UUID v7은 상위 48비트에 Unix Timestamp를 포함함으로 마치 트위터의 스노우플레이크 알고리즘처럼 DB의 B-Tree 인덱스를 타기 유연하게 설계되었습니다. 이는 Page Fragmentation을 방지하고 캐시 히트율을 최적화 하기 위함입니다

--- modules/common/src/main/java/com/hig/domain/common/BaseEntity.java
+++ modules/common/src/main/java/com/hig/domain/common/BaseEntity.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public abstract class BaseEntity {
 
     @Id
-    @UuidGenerator
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
     @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
 
