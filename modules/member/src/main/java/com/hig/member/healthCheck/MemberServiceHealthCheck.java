d8376fd390f56a147cf78abc61779dd085de5906
dlsrbsla52 <32616025+dlsrbsla52@users.noreply.github.com>
2026-03-13 15:21:31 +0900

    추가 개발 : 헬스체크 추가

--- modules/member/src/main/java/com/hig/member/healthCheck/MemberServiceHealthCheck.java
+++ modules/member/src/main/java/com/hig/member/healthCheck/MemberServiceHealthCheck.java
@@ -1,15 +1,16 @@
-package com.hig.auth.healthCheck;
+package com.hig.member.healthCheck;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AuthServiceHealthCheck {
+public class MemberServiceHealthCheck {
 
     @GetMapping("/health-check")
     public ResponseEntity<Void> healthCheck() {
         return ResponseEntity.ok().build();
     }
+
 
 }
